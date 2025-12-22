77e010adbae52d0d6e3eff2cf1034c80260e9e0f
rZcld7 <russellzuleikha6@gmail.com>
2025-12-22 17:35:53 -0300

    Estructura inicial

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(238, 187, 0, 1);
+  static const Color primaryLight = Color.fromRGBO(238, 187, 0, 0.61);
+  //colores de soporte
+  static const Color error = Color.fromRGBO(175, 21, 24, 1);
+  static const Color scaffoldBackground = Color.fromRGBO(255, 255, 255, 1);
+  static const Color backgroundInput = Color.fromRGBO(196, 196, 196, 0.2);
+  static const Color overlay = Color.fromRGBO(77, 77, 77, 1);
+}
---
+++ lib/utils/app_texts.dart
@@ -0,0 +1,51 @@
+class AppTexts {
+  //Screen pantalla carga
+  static const String loadingTitle = "Cargando por favor espere...";
+  //Screen de registro
+  static const String registerTitle = "Registro de cuenta";
+  static const String registerSubtitle = "Regístrate con tus datos personales";
+  static const String hintInputNameAndLastNameRegister = "Nombre y Apellido";
+  static const String hintInputEmailRegister = "Correo electrónico";
+  static const String hintInputPasswordRegister = "Contraseña";
+  static const String textButtonHaveAccount =
+      "¿Ya tienes una cuenta? Inicia sesión aquí";
+  static const String registerButton = "Registrarse";
+
+  //Screen login
+  static const String loginTitle = "Bienvenido/a";
+  static const String loginSubtitle = "Accede a tu cuenta";
+  static const String hintInputEmailLogin = "Ingresa tu correo";
+  static const String hintInputPasswordLogin = "Contraseña";
+  static const String checkboxRememberLogin = "Recuérdame";
+  static const String textButtonForgotPassword = "¿Olvidaste tu contraseña?";
+  static const String loginButton = "Iniciar sesión";
+
+  //Screen recuperar
+  static const String resetTitle = "Reestablecer contraseña";
+  static const String resetSubtitle =
+      "Introduce tu correo para obtener un código de verificación";
+  static const String hintInputEmailReset = "Ingresa tu correo";
+  static const String resetButton = "Solicitar";
+
+  //Screen verificar número
+  static const String verifyNumberTitle = "Verificación";
+  static const String verifyNumberInstruction =
+      "Introduce el código de verificación enviado al número +56 9XXXXXXXX";
+  static const String hintVerifyNumberCode = "0";
+  static const String verifyNumberButton = "Verificar";
+  static const String resendNumberCode = "¿No recibiste un código?";
+  static const String cooldownResendNumberCode =
+      "Solicita un nuevo código en 00:30s";
+
+  //Confirmación correo
+  static const String verifyEmailTitle = "Confirmación de correo";
+  static const String verifyEmailSubtitle = "Confirma tu correo para seguir";
+  static const String verifyEmailInstruction =
+      "Hace clic en el encale que te hemos enviado a tu correo example@gmail.com para confirmar que el correo te pertenece";
+  static const String resendEmailText = "¿No te llegó el correo?";
+  static const String resendVerifyEmailButton = "Reenviar";
+  static const String cooldownResendVerifyEmail =
+      "Solicita un nuevo correo en 00:30s";
+  static const String confirmEmailText = "¿Ya confirmaste?";
+  static const String verifyEmailButton = "Siguiente";
+}
